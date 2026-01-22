ca6a7ef45f8a8a943862b5a7ecaf554a98e07fd6
ryanseery <ryanpseery@gmail.com>
2026-01-21 20:36:37 -0500

    feat: eslint no unused styles

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const reactNative = require('eslint-plugin-react-native');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: {
+      'react-native': reactNative,
+    },
     rules: {
+      'react-native/no-unused-styles': 'warn',
       'import/order': [
         'warn',
         {
