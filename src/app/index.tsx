1644f1742526c746213d7e97aeafc1855987105f
ryanseery <ryanpseery@gmail.com>
2026-01-23 00:02:45 -0500

    title screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
 import { StyleSheet, Text, View } from 'react-native';
 
+const options = ['Game', 'Movie', 'Book'];
+
+const fontColor = '#333';
+
 export default function Index() {
+  const [what, setWhat] = useState('Game');
+
+  const onValueChange = (v: string) => setWhat(v);
+
   return (
     <View style={styles.root}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={what}
+          itemStyle={styles.pickerItem}
+          onValueChange={onValueChange}
+        >
+          {options.map((option) => (
+            <Picker.Item key={option} label={option} value={option} />
+          ))}
+        </Picker>
+      </View>
+      <Text style={styles.ofTheText}>of the</Text>
+      <Text style={styles.yearText}>Year</Text>
     </View>
   );
 }
@@ -11,7 +33,29 @@ export default function Index() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pickerContainer: {
+    width: '100%',
+    overflow: 'hidden',
+  },
+  pickerItem: {
+    fontSize: 72,
+    fontWeight: 'bold',
+    height: 88,
+  },
+  ofTheText: {
+    fontSize: 52,
+    fontWeight: '700',
+    color: fontColor,
+    marginVertical: 16,
+  },
+  yearText: {
+    fontSize: 72,
+    fontWeight: 'bold',
+    color: fontColor,
   },
 });
